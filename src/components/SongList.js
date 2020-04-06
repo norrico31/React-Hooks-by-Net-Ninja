a46fd30e3ad2b4bd1802908c9d096cdec60286ce
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-04-06 20:45:39 +0800

    using useState hook inside functional component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,29 @@
 import React, { useState } from 'react';
+import uuid from 'react-uuid';
 
 const SongList = () => {
+
+    // it takes 2 arguments. 1st argument return the data, 2nd argument a function to change the data 
+    const [ songs, setSongs ] = useState([
+        { title: 'almost home', id: 1 },
+        { title: 'memory gospel', id: 2 },
+        { title: 'this wild darkness', id: 3 }
+    ]);
+
+    const addSong = () => {
+        setSongs([ ...songs, { title: 'new song', id: uuid() }]);
+    }
+
     return (
         <div className="song-list">
             <ul>
-                <li>This Wild Darkness</li>
-                <li>Memory Gospel</li>
+                {songs.map((song) => {
+                    return (
+                        <li key={ song.id }>{ song.title }</li>
+                    )
+                })}
             </ul>
+            <button onClick={ addSong }>Add a song</button>
         </div>
     )
 }
